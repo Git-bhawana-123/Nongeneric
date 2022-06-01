6e1d131c93b4b0736e5df3c35efa65135eff8919
Bhawana <bhawana.r@zensar.com>
2022-06-01 17:47:40 +0530

    first commit

---
+++ NonGenreicClassAssignment/HashtableAssignment.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace NonGenreicClassAssignment
+{
+    internal class HashtableAssignment
+    {
+        static void Main()
+        {
+            
+            var myHT = new Hashtable();
+            myHT.Add(0, "zero");
+            myHT.Add(1, "one");
+            myHT.Add(2, "two");
+            myHT.Add(3, "three");
+            myHT.Add(4, "four");
+            foreach (DictionaryEntry entry in myHT)
+            {
+                Console.WriteLine($"{entry.Key}=={entry.Value}");
+
+            }
+            
+            Console.WriteLine("After removing");
+            myHT.Remove(1);
+            foreach (DictionaryEntry de in myHT)
+            {
+                Console.WriteLine($"{de.Key}=={de.Value}");
+
+            }
+                
+            Console.WriteLine();
+
+            int value=4;
+            Console.WriteLine("contains: {0} is {1}", value,myHT.Contains(value));
+            
+            
+
+            int myKey = 2;
+            Console.WriteLine("The key: {0} is {1}", myKey, myHT.ContainsKey(2));
+
+            
+
+            var myValue = "three";
+            Console.WriteLine("The value: {0} is {1}", myValue, myHT.ContainsValue(myValue) );
+            
+            
+
+
+           
+            var myTargetArray = new String[15];
+            myTargetArray[0] = "The";
+            myTargetArray[1] = "quick";
+            myTargetArray[2] = "brown";
+            myTargetArray[3] = "fox";
+            myTargetArray[4] = "jumps";
+            myTargetArray[5] = "over";
+            myTargetArray[6] = "the";
+            myTargetArray[7] = "lazy";
+            myTargetArray[8] = "dog";
+
+            
+           
+            string[] copyData = new string[myTargetArray.Length];
+            Console.WriteLine("After copying the values, starting at index 6:");
+            myTargetArray.CopyTo(myTargetArray, 0);
+
+            foreach (var item in myTargetArray)
+            {
+                Console.WriteLine(item);
+
+            }
+            Console.WriteLine("After clonning the values");
+            var output = (Array)myTargetArray.Clone();
+            foreach (var item in output)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+       
+
+
+        
+
+        
+    }
+}
---
+++ NonGenreicClassAssignment/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace NonGenreicClassAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList mySL = new SortedList();
+            mySL.Add("one", "The");
+            mySL.Add("two", "quick");
+            mySL.Add("three", "brown");
+            mySL.Add("four", "fox");
+
+            String[] temArray = new string[] { "The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog" };
+            DictionaryEntry[] myTargetArray = new DictionaryEntry[15];
+            int i = 0;
+            foreach (string s in temArray)
+            {
+                myTargetArray[i].Key = i;
+                myTargetArray[i].Value = s;
+                i++;
+            }
+
+            Console.WriteLine("The SortedList contains the following:");
+            var iterate = mySL.GetEnumerator();
+            IDictionaryEnumerator vs = mySL.GetEnumerator();//correct
+            while (iterate.MoveNext())
+            {
+                Console.WriteLine($"{iterate.Key} == {iterate.Value}");
+            }
+
+
+
+
+
+            SortedList my_slist = new SortedList();
+
+            // Adding key/value pairs in
+            // SortedList using Add() method
+            my_slist.Add(1.02, "This");
+            my_slist.Add(1.07, "Is");
+            my_slist.Add(1.04, "SortedList");
+            my_slist.Add(1.01, "Tutorial");
+
+            // Using Contains() method to check
+            // the specified key is present or not
+            if (my_slist.Contains(1.02) == true)
+            {
+                Console.WriteLine("Key is found...!!");
+            }
+
+            else
+            {
+                Console.WriteLine("Key is not found...!!");
+            }
+
+            // Using ContainsKey() method to check
+            // the specified key is present or not
+            if (my_slist.ContainsKey(1.03) == true)
+            {
+                Console.WriteLine("Key is found...!!");
+            }
+            else
+            {
+                Console.WriteLine("Key is not found...!!");
+            }
+
+            // Using ContainsValue() method to check
+            // the specified value is present or not
+            if (my_slist.ContainsValue("Is") == true)
+            {
+                Console.WriteLine("Value is found...!!");
+            }
+
+            else
+            {
+                Console.WriteLine("Value is not found...!!");
+            }
+
+
+
+
+            SortedList list = new SortedList();
+
+           
+            list.Add(1.02, "This");
+            list.Add(1.07, "Is");
+            list.Add(1.04, "SortedList");
+            list.Add(1.01, "Tutorial");
+
+            foreach (DictionaryEntry pair in list)
+            {
+                Console.WriteLine("{0} and {1}",
+                          pair.Key, pair.Value);
+            }
+            Console.WriteLine();
+
+           
+            list.Remove(1.07);
+
+            // After Remove() method
+            foreach (DictionaryEntry pair in list)
+            {
+                Console.WriteLine("{0} and {1}",
+                          pair.Key, pair.Value);
+            }
+            Console.WriteLine();
+
+            // Remove element at index 2
+            my_slist.RemoveAt(2);
+
+            // After RemoveAt() method
+            foreach (DictionaryEntry pair in list)
+            {
+                Console.WriteLine("{0} and {1}",
+                          pair.Key, pair.Value);
+            }
+            Console.WriteLine();
+
+           
+            list.Clear();
+            Console.WriteLine("The total number of key/value pairs" +
+                        " present in my_slist:{0}", list.Count);
+        }
+    }
+}
